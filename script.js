7f9549b25b1d9a0eab032daeecbb2671026d5487
João Camilo M <jcamilomallmann@hotmail.com>
2022-06-21 19:41:24 -0300

    .

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 const $html = document.querySelector("html");
+const $nav = document.querySelector("nav");
 
 Botaou.addEventListener("change", () => {
   $html.classList.toggle("light-mode");
+});
+
+check.addEventListener("change", () => {
+  $nav.classList.toggle("desativado");
 });
